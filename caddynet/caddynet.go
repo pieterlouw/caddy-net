0c21236cda07a93d9790d1434e9a9442cac06165
pieterlouw <pieterlouw@gmail.com>
2017-03-17 10:20:14 +0200

    Recurring changes

--- caddynet/caddynet.go
+++ caddynet/caddynet.go
@@ -3,8 +3,5 @@ package caddynet
 import (
 	// plug in the server
 	_ "github.com/pieterlouw/caddy-net/caddynet/netserver"
-
-	// plug in the standard directives
-	_ "github.com/pieterlouw/caddy-net/caddynet/echo"
-	_ "github.com/pieterlouw/caddy-net/caddynet/proxy"
+	// no directives at this stage
 )
